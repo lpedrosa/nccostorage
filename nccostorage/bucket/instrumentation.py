e0123fa4654f0e65382c0f99f14c7d4e395c9ffa
lpedrosa <lpedrosa88@gmail.com>
2018-05-13 23:33:52 +0100

    add storage read metrics to the bucket storage

--- nccostorage/bucket/instrumentation.py
+++ nccostorage/bucket/instrumentation.py
@@ -1,4 +1,4 @@
-from prometheus_async.aio import time, count_exceptions
+from prometheus_async.aio import count_exceptions, time
 from prometheus_client import Counter, Gauge, Histogram
 
 
@@ -7,11 +7,17 @@ LIVE_BUCKETS = Gauge('live_buckets', 'number of live buckets in storage')
 # pylint: disable-msg=no-value-for-parameter
 LIVE_NCCOS = Gauge('live_nccos', 'number of live nccos in storage')
 # pylint: disable-msg=no-value-for-parameter
-UPDATE_REQUEST = Counter('update_count', 'bucket update method call rate')
+UPDATE_REQUEST = Counter('update_count', 'storage update method call rate')
 # pylint: disable-msg=no-value-for-parameter
-UPDATE_ERROR = Counter('update_error', 'bucket update error rate')
+UPDATE_ERROR = Counter('update_error', 'storage update error rate')
 # pylint: disable-msg=no-value-for-parameter
-UPDATE_TIME = Histogram('update_time', 'bucket update request latency (in seconds)')
+UPDATE_TIME = Histogram('update_time', 'storage update request latency (in seconds)')
+# pylint: disable-msg=no-value-for-parameter
+READ_REQUEST = Counter('read_count', 'storage read method call rate')
+# pylint: disable-msg=no-value-for-parameter
+READ_ERROR = Counter('read_error', 'storage read error rate')
+# pylint: disable-msg=no-value-for-parameter
+READ_TIME = Histogram('read_time', 'storage read request latency (in seconds)')
 
 
 class InstrumentedBucketStorage(object):
@@ -46,8 +52,11 @@ class InstrumentedBucketStorage(object):
             LIVE_NCCOS.inc()
             return result
 
+    @time(READ_TIME)
     async def get_ncco(self, bucket_name, ncco_id):
-        return await self._storage.get_ncco(bucket_name, ncco_id)
+        READ_REQUEST.inc()
+        with READ_ERROR.count_exceptions():
+            return await self._storage.get_ncco(bucket_name, ncco_id)
 
     @time(UPDATE_TIME)
     async def remove_ncco(self, bucket_name, ncco_id):
